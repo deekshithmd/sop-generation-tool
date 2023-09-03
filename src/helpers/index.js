1ac2af37454f4879fd9e56ceeaef67325a03e3e1
deekshithepns <70252174+deekshithmd@users.noreply.github.com>
2023-09-03 13:19:18 +0530

    updated backend

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ENDPOINT = "http://localhost:5000/";
+const API_ENDPOINT = "https://sop-tool-backend.vercel.app/";
 
 export const validateEmail = (email) => {
     var re = /\S+@\S+\.\S+/;
